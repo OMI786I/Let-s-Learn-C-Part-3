bd9a2204df7f35e11ff2e2d79c137383a670fcdc
OMI786I <sohrawardy1998@gmail.com>
2026-08-20 23:35:43 +0600

    conditional operator

--- Program.cs
+++ Program.cs
@@ -3,3 +3,31 @@
 int discount = saleAmount > 1000? 100 : 50;
 
 Console.WriteLine($"Discount applied: {discount}");
+
+
+
+int coin = new Random().Next(0,2);
+
+string value = coin == 0? "Heads" : "Tails";
+
+Console.WriteLine($"Coin flip result: {value}");
+
+
+
+//exercise to show different messages accordingt o different levels
+
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin"))
+{
+    Console.WriteLine("You have admin privileges.");
+}
+else if (permission.Contains("Manager"))
+{
+    Console.WriteLine("You have manager privileges.");
+}
+else
+{
+    Console.WriteLine("You have standard user privileges.");
+}
--- obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca23c1dc7707eb03c0610e3831fac43457f15bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a18338dbdeb3520041c409d5493f7c85b284883")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
