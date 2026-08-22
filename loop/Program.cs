cab5673e04dd3e6968a35a5d5ad7348643d79bd3
OMI786I <sohrawardy1998@gmail.com>
2026-08-22 16:37:29 +0600

    a normal game using loop and if-elsse

--- loop/Program.cs
+++ loop/Program.cs
@@ -1,4 +1,5 @@
-﻿string [] names = ["John", "Jane", "Jack", "Jill"];
+﻿/*
+string [] names = ["John", "Jane", "Jack", "Jill"];
 
 for (int i = 0; i<= names.Length-1; i++)
 {
@@ -36,4 +37,92 @@ for (int i = 1; i<=100; i++)
  {
     Console.WriteLine(i);
  }
+}
+*/
+
+//do while loop
+
+/*
+Random number = new();
+int currentNumber;
+do
+{
+    currentNumber = number.Next(1, 11);
+    Console.WriteLine(currentNumber);
+} while (currentNumber != 7);
+
+*/
+/*
+Random random = new ();
+int current = random.Next(1, 11);
+
+/*
+do
+{
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
+*/
+
+/*
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
+
+*/
+
+/*
+do
+{
+    current = random.Next(1, 11);
+
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
+
+
+*/
+
+
+
+// exercise: an rpg game. player starts with 10 health and mosnter also has 10 health. print the outcome below
+
+
+int playerHealth = 10;
+int monsterHealth = 10;
+int round = 0;
+Random random = new ();
+int attacks;
+
+while (playerHealth > 0 && monsterHealth > 0)
+{   round ++;
+    attacks = random.Next(1, 11);
+if (round % 2 == 0)
+    {
+
+          playerHealth -= attacks;
+        Console.WriteLine($"Round: {round}\n Monster attacks player for {attacks} damage. Player health is now {playerHealth}"); 
+       
+    }
+
+    else
+    {
+         monsterHealth -= attacks;
+    Console.WriteLine($"Round: {round}\nPlayer attacks monster for {attacks} damage. Monster health is now {monsterHealth}"); 
+      
+    }
+   
+}
+
+if (playerHealth <= 0)
+{
+    Console.WriteLine("Monster wins!");
+}
+else
+{
+    Console.WriteLine("Player wins!");
 }
